df218cde25be5ea05728c6ca35459b956e518186
Sergey Stepanets <stepanetssergey@gmail.com>
2023-03-18 11:51:35 +0200

    change community

--- lesson2/hardhat/contracts/Community.sol
+++ lesson2/hardhat/contracts/Community.sol
@@ -6,19 +6,42 @@ contract Community {
  
     string public CommunityName;
 
-    mapping(address => bool) public activeUsers;
+    uint matchId;
 
-    //function addMatch(........)
-
-
-    //function addMatchResult(.......)
-
-
-    //function voteValidator(.......)
-
-
-    function signUp() public {
-        activeUsers[msg.sender] = true;
+    struct Match {
+        string name;
+        string part1;
+        string part2;
+        uint scorePart1;
+        uint scorePart2;
+        uint startDate;
     }
+
+
+    struct User {
+        uint balance;
+    }
+
+    mapping(uint => Match) internal Matches;
+    mapping(address => User) internal Users;
+    
+    function getMatch(uint _id) public view returns(Match memory) {
+        return Matches[_id];
+    }
+
+    function addMatch(string memory _name, string memory _part1, string memory _part2, uint _startDate) public {
+        matchId += 1;
+        Matches[matchId].name = _name;
+        Matches[matchId].part1 = _part1;
+        Matches[matchId].part2 = _part2;
+        Matches[matchId].startDate = _startDate;
+
+    }
+
+    function signIn() public {
+        Users[msg.sender].balance = 0;
+    }
+
+
      
 }
--- lesson2/hardhat/contracts/ERC20.sol
+++ lesson2/hardhat/contracts/ERC20.sol
@@ -8,14 +8,22 @@ contract ERC20 {
    string public _name;
    string public _symbol;
    uint public _totalSupply;
+   address public owner;
 
    mapping(address => uint) public balances;
    mapping(address => mapping(address => uint)) public allowens;
+   mapping(address => bool) public Minters;
   constructor(string memory tokenName, string memory tokenSymbol, uint tokenTotalSupply) {
       _name = tokenName;
       _symbol = tokenSymbol;
       _totalSupply = tokenTotalSupply;
       balances[msg.sender] = tokenTotalSupply;
+      owner = msg.sender;
+  }
+
+  function setMinter(address _address) public {
+    require(msg.sender == owner,'only owner');
+    Minters[_address] = true;
   }
   function name() public view returns(string memory) {
     return _name;
@@ -36,6 +44,30 @@ contract ERC20 {
     balances[_to] = balances[_to] + _amount;
     return true;
   }
+
+
+  function approve(address _sender, uint _amount) public  {
+    allowens[msg.sender][_sender] = _amount;
+  }
+
+  function transferFrom(address _from, address _to, uint _amount) public {
+    require(allowens[_from][_to] >= _amount,'not enough amount');
+    balances[_from] = balances[_from] - _amount;
+    balances[_to] += _amount;
+  }
+
+  function mint(address _to, uint _amount) public {
+    require(Minters[msg.sender] == true, 'Only minter can do it');
+    //balances[_to] = balances[_to] + _amount;
+    uint i = 1;
+    i = i + 1;
+    i++;
+    //_totalSupply = _totalSupply + _amount;
+    _totalSupply += _amount;
+    balances[_to] += _amount;
+  }
+
+
 }  
   //function blanceOf(address _address) public view returns(address)
 
